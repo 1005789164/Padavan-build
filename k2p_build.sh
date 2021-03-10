cf3088a0dce92e6e4ad6d1790213b1a50e313c59
MJ <1005789164@qq.com>
2021-03-10 18:16:03 +0800

    update

--- k2p_build.sh
+++ k2p_build.sh
@@ -14,14 +14,27 @@
 	#########################################################################################
 	sed -i 's/CONFIG_FIRMWARE_INCLUDE_CURL=.*/CONFIG_FIRMWARE_INCLUDE_CURL=y/' .config
 	sed -i 's/CONFIG_FIRMWARE_INCLUDE_NAPT66=.*/CONFIG_FIRMWARE_INCLUDE_NAPT66=y/' .config
-	sed -i 's/CONFIG_FIRMWARE_INCLUDE_DNSFORWARDER=.*/CONFIG_FIRMWARE_INCLUDE_DNSFORWARDER=y/' .config
-	sed -i 's/CONFIG_FIRMWARE_INCLUDE_IPERF3=.*/CONFIG_FIRMWARE_INCLUDE_IPERF3=y/' .config
-	sed -i 's/CONFIG_FIRMWARE_INCLUDE_TTYD=.*/CONFIG_FIRMWARE_INCLUDE_TTYD=y/' .config
+	sed -i 's/CONFIG_FIRMWARE_INCLUDE_DNSFORWARDER=.*/CONFIG_FIRMWARE_INCLUDE_DNSFORWARDER=n/' .config
+	sed -i 's/CONFIG_FIRMWARE_INCLUDE_IPERF3=.*/CONFIG_FIRMWARE_INCLUDE_IPERF3=n/' .config
+	sed -i 's/CONFIG_FIRMWARE_INCLUDE_TTYD=.*/CONFIG_FIRMWARE_INCLUDE_TTYD=n/' .config
+	sed -i 's/CONFIG_FIRMWARE_INCLUDE_NANO=.*/CONFIG_FIRMWARE_INCLUDE_NANO=n/' .config
+	sed -i 's/CONFIG_FIRMWARE_INCLUDE_MTR=.*/CONFIG_FIRMWARE_INCLUDE_MTR=n/' .config
+	sed -i 's/CONFIG_FIRMWARE_INCLUDE_SOCAT=.*/CONFIG_FIRMWARE_INCLUDE_SOCAT=n/' .config
+	sed -i 's/CONFIG_FIRMWARE_INCLUDE_SRELAY=.*/CONFIG_FIRMWARE_INCLUDE_SRELAY=n/' .config
+	sed -i 's/CONFIG_FIRMWARE_INCLUDE_HTOP=.*/CONFIG_FIRMWARE_INCLUDE_HTOP=n/' .config
+	sed -i 's/CONFIG_FIRMWARE_INCLUDE_OPENSSH=.*/CONFIG_FIRMWARE_INCLUDE_OPENSSH=n/' .config
+	sed -i 's/CONFIG_FIRMWARE_INCLUDE_DROPBEAR=.*/CONFIG_FIRMWARE_INCLUDE_DROPBEAR=y/' .config
+	sed -i 's/CONFIG_FIRMWARE_INCLUDE_DROPBEAR_FAST_CODE=.*/CONFIG_FIRMWARE_INCLUDE_DROPBEAR_FAST_CODE=y/' .config
 	#########################################################################################
 	
-	################################################################################################
+	#########################################################################################
+	#自定义添加其它功能请参考源码configs/templates/目录下的config文件。按照上面的格式添加即可
+	#格式如下：
+	#sed -i '/自定义项/d' .config
+	#echo "自定义项=y" >> .config
+	#########################################################################################
 	#因不同型号配置功能不一样，所以先把配置项删除，如果你自己要添加其他的，也要写上删除这一条，切记！！！
-	################################################################################################
+	#########################################################################################
 	sed -i '/CONFIG_FIRMWARE_INCLUDE_MENTOHUST/d' .config #删除配置项MENTOHUST
 	sed -i '/CONFIG_FIRMWARE_INCLUDE_SCUTCLIENT/d' .config #删除配置项SCUTCLIENT
 	sed -i '/CONFIG_FIRMWARE_INCLUDE_SHADOWSOCKS/d' .config #删除配置项SS plus+
@@ -35,15 +48,15 @@
 	sed -i '/CONFIG_FIRMWARE_INCLUDE_SMARTDNS/d' .config
 	sed -i '/CONFIG_FIRMWARE_INCLUDE_SRELAY/d' .config
 	sed -i 's/CONFIG_FIRMWARE_INCLUDE_OPENSSL_EXE=n/CONFIG_FIRMWARE_INCLUDE_OPENSSL_EXE=y/g' .config
-	######################################################################
+	#########################################################################################
 	#以下选项是定义你需要的功能（y=集成,n=忽略），重新写入到.config文件
-	######################################################################
+	#########################################################################################
 	echo "CONFIG_FIRMWARE_INCLUDE_MENTOHUST=n" >> .config #MENTOHUST
 	echo "CONFIG_FIRMWARE_INCLUDE_SCUTCLIENT=n" >> .config #SCUTCLIENT
 	echo "CONFIG_FIRMWARE_INCLUDE_SHADOWSOCKS=y" >> .config #SS plus+
 	echo "CONFIG_FIRMWARE_INCLUDE_SSOBFS=y" >> .config # simple-obfs混淆插件
-	echo "CONFIG_FIRMWARE_INCLUDE_SSSERVER=y" >> .config #SS server
-	echo "CONFIG_FIRMWARE_INCLUDE_DNSFORWARDER=y" >> .config #DNSFORWARDER
+	echo "CONFIG_FIRMWARE_INCLUDE_SSSERVER=n" >> .config #SS server
+	echo "CONFIG_FIRMWARE_INCLUDE_DNSFORWARDER=n" >> .config #DNSFORWARDER
 	echo "CONFIG_FIRMWARE_INCLUDE_ADBYBY=n" >> .config #adbyby plus+
 	echo "CONFIG_FIRMWARE_INCLUDE_FRPC=n" >> .config #内网穿透FRPC
 	echo "CONFIG_FIRMWARE_INCLUDE_FRPS=n" >> .config #内网穿透FRPS
@@ -54,20 +67,15 @@
 	echo "CONFIG_FIRMWARE_INCLUDE_V2RAY=y" >> .config #集成v2ray执行文件（3.8M左右)，如果不集成，会从网上下载下来执行，不影响正常使用
 	echo "CONFIG_FIRMWARE_INCLUDE_TROJAN=y" >> .config #集成trojan执行文件(1.1M左右)，如果不集成，会从网上下载下来执行，不影响正常使用
 	echo "CONFIG_FIRMWARE_INCLUDE_KOOLPROXY=n" >> .config #KP广告过滤
-	echo "CONFIG_FIRMWARE_INCLUDE_CADDY=y" >> .config #在线文件管理服务
+	echo "CONFIG_FIRMWARE_INCLUDE_CADDY=n" >> .config #在线文件管理服务
 	echo "CONFIG_FIRMWARE_INCLUDE_CADDYBIN=n" >> .config #集成caddu执行文件，此文件有13M,请注意固件大小。如果不集成，会从网上下载下来执行，不影响正常使用
 	echo "CONFIG_FIRMWARE_INCLUDE_ADGUARDHOME=n" >> .config
 	echo "CONFIG_FIRMWARE_INCLUDE_ADGUARDHOMEBIN=n" >> .config
 	#此文件有15M,请注意固件大小。如果不集成，会从网上下载下来执行，不影响正常使用
-	echo "CONFIG_FIRMWARE_INCLUDE_SRELAY=y" >> .config #可以不集成
+	echo "CONFIG_FIRMWARE_INCLUDE_SRELAY=n" >> .config #可以不集成
 	echo "CONFIG_FIRMWARE_INCLUDE_WYY=y" >> .config #网易云解锁
 	echo "CONFIG_FIRMWARE_INCLUDE_WYYBIN=y" >> .config #网易云解锁GO版本执行文件（4M多）注意固件超大小,不集成会自动下载
 	echo "CONFIG_FIRMWARE_INCLUDE_ZEROTIER=n" >> .config #zerotier ~1.3M
-	#########################################################################################
-	#自定义添加其它功能请参考源码configs/templates/目录下的config文件。按照上面的格式添加即可
-	#格式如下：
-	#sed -i '/自定义项/d' .config
-	#echo "自定义项=y" >> .config
 	#########################################################################################
 	
 	sudo ./build_firmware_modify $1 0
